9aa3305b1bc080fa5290255611058cb2b14667ed
Chris Rehn <crehn@outlook.com>
2022-01-13 00:29:08 -0800

    Retry with React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import React from "react";
+import * as ed25519 from "@noble/ed25519";
+import { Container } from "react-bootstrap";
 
-export function App() {
-  return <h1>Hello from React!</h1>;
+async function generateEd25519KeyPair() {
+  const privateKey = ed25519.utils.randomPrivateKey();
+  const publicKey = await ed25519.getPublicKey(privateKey);
+  return { privateKey, publicKey };
+}
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    const { privateKey, publicKey } = await generateEd25519KeyPair();
+    this.setState({
+      privateKey: ed25519.utils.bytesToHex(privateKey),
+      publicKey: ed25519.utils.bytesToHex(publicKey),
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <code>
+          {this.state.privateKey} {this.state.publicKey}
+        </code>
+      </Container>
+    );
+  }
 }
