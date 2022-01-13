4ce33e003c7515501d5f73b64b1c873da4a388f2
Chris Rehn <crehn@outlook.com>
2022-01-13 00:39:02 -0800

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from "react";
 import * as ed25519 from "@noble/ed25519";
 import { Container } from "react-bootstrap";
 
-async function generateEd25519KeyPair() {
-  const privateKey = ed25519.utils.randomPrivateKey();
-  const publicKey = await ed25519.getPublicKey(privateKey);
-  return { privateKey, publicKey };
-}
-
 export class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,7 +9,9 @@ export class App extends React.Component {
   }
 
   async componentDidMount() {
-    const { privateKey, publicKey } = await generateEd25519KeyPair();
+    const privateKey = ed25519.utils.randomPrivateKey();
+    console.log(privateKey);
+    const publicKey = await ed25519.getPublicKey(privateKey);
     this.setState({
       privateKey: ed25519.utils.bytesToHex(privateKey),
       publicKey: ed25519.utils.bytesToHex(publicKey),
